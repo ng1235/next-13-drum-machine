a0c17c6a17654f345f059da6928af6ff1719086b
Matthew Ng <matthewtng.2@gmail.com>
2023-10-19 15:34:41 +0900

    feat: complete exercise

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,31 @@
-import React from 'react';
+"use client";
+import React from "react";
 
-import Header from '../components/Header';
+import Header from "../components/Header";
 
-import './styles.css';
+import "./styles.css";
+
+export const SoundEnabledContext = React.createContext();
 
 function RootLayout({ children }) {
+  const [isSoundEnabled, setIsSoundEnabled] = React.useState(true);
+  const value = {
+    isSoundEnabled,
+    setIsSoundEnabled,
+  };
   return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-        <footer>
-          <img src="/ie-badge.gif" width={100} />
-          <span>Thanks for visiting!</span>
-        </footer>
-      </body>
-    </html>
+    <SoundEnabledContext.Provider value={value}>
+      <html lang="en">
+        <body>
+          <Header />
+          {children}
+          <footer>
+            <img src="/ie-badge.gif" width={100} />
+            <span>Thanks for visiting!</span>
+          </footer>
+        </body>
+      </html>
+    </SoundEnabledContext.Provider>
   );
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,31 @@
-import React from 'react';
+"use client";
+import React from "react";
 
-import MaxWidthWrapper from '../components/MaxWidthWrapper';
-import DrumMachine from '../components/DrumMachine';
-import styles from './page.module.css';
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import DrumMachine from "../components/DrumMachine";
+import styles from "./page.module.css";
+import { SoundEnabledContext } from "./layout";
 
 function Home() {
+  const { isSoundEnabled, setIsSoundEnabled } =
+    React.useContext(SoundEnabledContext);
   return (
-    <MaxWidthWrapper
-      as="main"
-      className={styles.wrapper}
-    >
-      <h1>
-        Welcome to the zaniest place on the ‘Web!
-      </h1>
+    <MaxWidthWrapper as="main" className={styles.wrapper}>
+      <h1>Welcome to the zaniest place on the ‘Web!</h1>
       <p>
-        Hey there, fellow netizens! If you're
-        reading this, you've stumbled upon the
-        coolest corner of the internet: my awesome
-        website. Get ready to embark on a
-        nostalgic journey back to the groovy era
-        of the 90s, where the web was wild, and
-        creativity ran free!
+        Hey there, fellow netizens! If you're reading this, you've stumbled upon
+        the coolest corner of the internet: my awesome website. Get ready to
+        embark on a nostalgic journey back to the groovy era of the 90s, where
+        the web was wild, and creativity ran free!
       </p>
       <p>
-        I've created the gnarliest drum machine
-        ever. Press the buttons to hear the music.
-        Tubular!
+        I've created the gnarliest drum machine ever. Press the buttons to hear
+        the music. Tubular!
       </p>
-      <DrumMachine />
+      <DrumMachine
+        isSoundEnabled={isSoundEnabled}
+        setIsSoundEnabled={setIsSoundEnabled}
+      />
     </MaxWidthWrapper>
   );
 }
