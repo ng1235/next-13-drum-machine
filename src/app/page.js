45a4e8b3ec7e084290d73adb7c1e7494a1768b80
Matthew Ng <matthewtng.2@gmail.com>
2023-10-19 15:36:52 +0900

    feat: remove extraneous lines

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 
 import MaxWidthWrapper from "../components/MaxWidthWrapper";
@@ -7,8 +6,6 @@ import styles from "./page.module.css";
 import { SoundEnabledContext } from "./layout";
 
 function Home() {
-  const { isSoundEnabled, setIsSoundEnabled } =
-    React.useContext(SoundEnabledContext);
   return (
     <MaxWidthWrapper as="main" className={styles.wrapper}>
       <h1>Welcome to the zaniest place on the ‘Web!</h1>
@@ -22,10 +19,7 @@ function Home() {
         I've created the gnarliest drum machine ever. Press the buttons to hear
         the music. Tubular!
       </p>
-      <DrumMachine
-        isSoundEnabled={isSoundEnabled}
-        setIsSoundEnabled={setIsSoundEnabled}
-      />
+      <DrumMachine />
     </MaxWidthWrapper>
   );
 }
